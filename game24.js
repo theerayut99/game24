a53286e4cd3f88d6d20f276e74fd5186a26ae641
Theerayut Thanyaphoo <theerayut_than@hotmail.com>
2023-06-09 16:46:38 +0700

    Create game24.js
    add game24

---
+++ game24.js
@@ -0,0 +1,89 @@
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateGame24() {
+  const numbers = [];
+
+  // Generate four random numbers between 1 and 9
+  for (let i = 0; i < 4; i++) {
+    const randomNumber = generateRandomNumber(1, 9);
+    numbers.push(randomNumber);
+  }
+
+  return numbers;
+}
+
+function solveGame24(numbers) {
+  const operators = ['+', '-', '*', '/'];
+
+  // Generate all possible permutations of the four numbers
+  const permutations = permute(numbers);
+
+  // Iterate through each permutation and try different combinations of operators
+  for (let i = 0; i < permutations.length; i++) {
+    const permutation = permutations[i];
+
+    // Iterate through each possible operator combination
+    for (let j = 0; j < operators.length; j++) {
+      for (let k = 0; k < operators.length; k++) {
+        for (let l = 0; l < operators.length; l++) {
+          const operator1 = operators[j];
+          const operator2 = operators[k];
+          const operator3 = operators[l];
+
+          // Generate all possible expressions using the permutation and operators
+          const expressions = [
+            `(${permutation[0]} ${operator1} ${permutation[1]}) ${operator2} ${permutation[2]} ${operator3} ${permutation[3]}`,
+            `${permutation[0]} ${operator1} (${permutation[1]} ${operator2} ${permutation[2]}) ${operator3} ${permutation[3]}`,
+            `${permutation[0]} ${operator1} ${permutation[1]} ${operator2} (${permutation[2]} ${operator3} ${permutation[3]})`,
+            `(${permutation[0]} ${operator1} ${permutation[1]}) ${operator2} (${permutation[2]} ${operator3} ${permutation[3]})`,
+            `(${permutation[0]} ${operator1} ${permutation[1]} ${operator2} ${permutation[2]}) ${operator3} ${permutation[3]}`,
+          ];
+
+          // Evaluate each expression and check if it equals 24
+          for (let m = 0; m < expressions.length; m++) {
+            const expression = expressions[m];
+            const result = eval(expression);
+
+            if (result === 24) {
+              return expression;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return 'No solution found.';
+}
+
+// Function to generate all possible permutations of an array
+function permute(nums) {
+  const result = [];
+
+  function backtrack(nums, tempList) {
+    if (tempList.length === nums.length) {
+      result.push([...tempList]);
+    } else {
+      for (let i = 0; i < nums.length; i++) {
+        if (tempList.includes(nums[i])) continue;
+        tempList.push(nums[i]);
+        backtrack(nums, tempList);
+        tempList.pop();
+      }
+    }
+  }
+
+  backtrack(nums, []);
+
+  return result;
+}
+
+// Generate a new game
+const numbers = generateGame24();
+console.log('Numbers:', numbers);
+
+// Solve the game
+const solution = solveGame24(numbers);
+console.log('Solution:', solution);
